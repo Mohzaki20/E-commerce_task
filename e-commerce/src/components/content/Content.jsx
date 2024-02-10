5386ac8839c3b3238dcf3b862ec4779cf0e8eb89
Mohammad Zaki <mz.26925787@gmail.com>
2024-02-10 14:11:58 -0800

    complete quantity section

--- e-commerce/src/components/content/Content.jsx
+++ e-commerce/src/components/content/Content.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./css/main.css";
 import shirt from "../../images/163975.jpeg";
 import Smallshirt from "../../images/shirt.png";
@@ -6,25 +6,26 @@ import adidas from "../../images/adidas.svg";
 import goldStar from "../../images/goldStar.svg";
 import emptyStar from "../../images/emptyStar.svg";
 import man from "../../images/man.png";
+import plus from "../../images/plus.svg";
+import minus from "../../images/minus.svg";
+import leftArrow from "../../images/leftArrow.svg";
+import rightArrow from "../../images/rightArrow.svg";
 
 function Content() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((res) => setData(res));
-  }, []);
+  const [counter, setCounter] = useState(1);
   return (
     <div className="content">
       <div className="container">
         {/* {data.map((e) =>  */}
         <div className="images">
           <img src={shirt} alt="" width={500} />
-          <div>
+          <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
+            <img src={leftArrow} alt="" width={15} />
             <img src={Smallshirt} alt="" width={100} />
             <img src={Smallshirt} alt="" width={100} />
             <img src={Smallshirt} alt="" width={100} />
             <img src={Smallshirt} alt="" width={100} />
+            <img src={rightArrow} alt="" width={15} />
           </div>
         </div>
         <div className="info">
@@ -64,16 +65,25 @@ function Content() {
             >
               9,999 <span style={{ fontSize: "20px" }}>LE</span>
             </span>
-            <span style={{ color: "#A7A9AC", fontSize: "16px", fontWeight: "600",textDecoration:"line-through" }}>9,999 LE</span>
+            <span
+              style={{
+                color: "#A7A9AC",
+                fontSize: "16px",
+                fontWeight: "600",
+                textDecoration: "line-through",
+              }}
+            >
+              9,999 LE
+            </span>
             <span className="off">30% Off</span>
           </div>
           <div className="size">
-            <h3 style={{fontSize:"18px",fontWeight:"800"}}>Size</h3>
-              <span>Small</span>
-              <span>Medium</span>
-              <span>Large</span>
-              <span>X Large</span>
-              <span>XX Large</span>
+            <h3 style={{ fontSize: "18px", fontWeight: "800" }}>Size</h3>
+            <span>Small</span>
+            <span>Medium</span>
+            <span>Large</span>
+            <span>X Large</span>
+            <span>XX Large</span>
           </div>
           <div className="color">
             <h3>Color</h3>
@@ -83,6 +93,22 @@ function Content() {
             <span>
               <img src={man} alt="" />
             </span>
+          </div>
+          <div className="quantity">
+            <h3>Quantity</h3>
+            <div className="counter">
+              <button className="minus" onClick={() => counter === 1 ? null : setCounter(counter - 1)}>
+                <img src={minus} alt="" width={12} />
+              </button>
+              <div className="num">{counter}</div>
+              <button className="plus" onClick={() => setCounter(counter + 1)}>
+                <img src={plus} alt="" width={12} />
+              </button>
+            </div>
+            <div className="buttons">
+              <button className="addtoCart" >Add To Cart</button>
+              <button className="pickup">Pickup From Store</button>
+            </div>
           </div>
         </div>
         {/* )} */}
